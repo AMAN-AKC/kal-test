1ce7c7d186d14a8d2266a776009fd89ebea28b31
AMAN-AKC <amanchauhan10122004@gmail.com>
2025-12-11 01:41:00 +0530

    lab_2

--- xss_demo/xss_app/views.py
+++ xss_demo/xss_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils.html import escape
+import re
 
-# Create your views here.
+def vulnerable_xss(request):
+    """Demonstrates XSS vulnerability - UNSAFE"""
+    name = ""
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+    
+    context = {
+        'name': name,
+        'vulnerability_type': 'Vulnerable (Unsafe)'
+    }
+    return render(request, 'xss_app/vulnerable.html', context)
--- xss_demo/xss_project/settings.py
+++ xss_demo/xss_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'xss_app',
 ]
 
 MIDDLEWARE = [
